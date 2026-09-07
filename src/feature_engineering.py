42575c935dbcdd416efa70ad94233c5bd7169e1f
nikhil <nikhilmjgp@gmail.com>
2026-09-07 19:29:07 +0530

    Pipeline configured with dvc with params

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -25,22 +25,22 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
-# def load_params(params_path:'str') -> dict:
-#     """ Load the paramters from yaml file"""
-#     try:
-#         with open(params_path,'r') as file:
-#             params = yaml.safe_load(file)
-#         logger.debug('Parameters retrieved from %s',params_path)
-#         return params
-#     except FileNotFoundError:
-#         logger.error('File is not find:%s',params_path)
-#         raise
-#     except yaml.YAMLError as e:
-#         logger.error('YAML error :%s',e)
-#         raise
-#     except Exception as e:
-#         logger.error('Unexpected error :' 
-#         '%s',e)
+def load_params(params_path:'str') -> dict:
+    """ Load the paramters from yaml file"""
+    try:
+        with open(params_path,'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s',params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File is not find:%s',params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error :%s',e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error :' 
+        '%s',e)
 def load_data(file_path:str)-> pd.DataFrame:
    """  Load data from a CSV file """
    try:
@@ -95,9 +95,9 @@ def save_data(df: pd.DataFrame, file_path: str) -> None:
         
 def main():
     try:
-        # params = load_params(params_path='params.yaml')
-        # max_feature = params['feature_engineering']['max_features']
-        max_feature = 50
+        params = load_params(params_path='params.yaml')
+        max_feature = params['feature_engineering']['max_features']
+        # max_feature = 50
 
         train_data = load_data('./data/interim/train_processed.csv')
         test_data = load_data('./data/interim/test_processed.csv')
